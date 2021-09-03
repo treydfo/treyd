4b360431b51b8e3143c7f8a5d4dc52041b150c20
jonsigurd <jonsigurdvp@gmail.com>
2021-09-03 12:26:00 +0100

    Initial commit

--- treyd/Shared/TreydFeesConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace treyd.Shared
-{
-    public static class TreydFeesConfig
-    {
-        // Subscription Payment: 250,00 dkk
-        public static int SUBSCRIPTION_FEE_NORMAL = 349;
-        public static int SUBSCRIPTION_FEE_BEST = 549;
-    }
-}
